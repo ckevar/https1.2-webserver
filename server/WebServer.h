38404c847d061fd5a29afe5029d380da623d2c74
ckevar <ckevar@gmail.com>
2020-01-20 00:47:09 +0000

    new file:   server/Makefile 	new file:   server/TcpListener.cpp 	new file:   server/TcpListener.h 	new file:   server/WebServer.cpp 	new file:   server/WebServer.h 	new file:   server/main.cpp 	new file:   wwwroot/index.html

---
+++ server/WebServer.h
@@ -0,0 +1,31 @@
+#ifndef WEB_SERVER_H
+#define WEB_SERVER_H
+
+#include "TcpListener.h"
+#include <string>
+
+class WebServer : public TcpListener
+{
+public:
+
+	WebServer(const char* ipAddress, int port) :
+		TcpListener(ipAddress, port) { }
+
+protected:
+
+	// Handler for client connections
+	virtual void onClientConnected(int clientSocket);
+
+	// Handler for client disconnections
+	virtual void onClientDisconnected(int clientSocket);
+
+	// Handler for when a message is received from the client
+	//virtual void onMessageReceived(int clientSocket, const char* msg, int length);
+	virtual void onMessageReceived(SSL *clientSocket, const char *msg, int length);
+
+private:
+	std::string contentType;
+	// MIME Type genenrator
+	void MIMEType(std::string *rType);
+};
+#endif
