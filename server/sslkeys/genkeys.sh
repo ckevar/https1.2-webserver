956d94bd20f31943507565fdad0087fc2d8b277c
ckevar <ckevar@gmail.com>
2022-04-13 22:39:20 +0000

    modified:   server/sslkeys/genkeys.sh

--- server/sslkeys/genkeys.sh
+++ server/sslkeys/genkeys.sh
@@ -1,5 +1,5 @@
 # set CN your public IP for telegram purposes
-openssl req -newkey rsa:2048 -sha256 -nodes -keyout my-ssl-private.key -x509 -days 365 -out my-ssl-public.pem -subj "/C=US/ST=New York/L=Brooklyn/O=cibot company/CN=34.125.127.143"
+openssl req -newkey rsa:2048 -sha256 -nodes -keyout my-ssl-private.key -x509 -days 365 -out my-ssl-public.pem -subj "/C=US/ST=New York/L=Brooklyn/O=cibot company/CN=<Public IP or Common Name>"
 # upload certification
 # DOCUMENTATION FOR TELEGRAM: https://core.telegram.org/bots/webhooks#how-do-i-set-a-webhook-for-either-type
-curl -F "url=https://34.125.127.143/" -F "certificate=@sslkeys/my-ssl-public.pem" https://api.telegram.org/bot5213003357:AAFKr6LOAwalDB1KMDkEVjtnDhTJfCW3GiI/setWebhook
+curl -F "url=https://<IPublic IP>/" -F "certificate=@sslkeys/my-ssl-public.pem" https://api.telegram.org/bot<TELEGRAM API TOKEN>/setWebhook
